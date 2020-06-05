2861b24b4e0735d286aa6637dc79e766cf92540a
kazarmax <kazarmax@gmail.com>
2020-06-06 00:53:47 +0300

    added downloading as csv plus minor refactoring

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import requests
 import json
 import view_templates
+import retronote_file_manage as rfm
 import os
 
 
@@ -26,7 +27,7 @@ def main():
     response_url = request.form['response_url']
     command_params = request.form['text'].strip().split(" ", 1)
     channel_id = request.form['channel_id']
-    retro_file_name = get_retrofile_name(channel_id)
+    retro_file_name = rfm.get_retrofile_name(channel_id)
 
     ### When user invokes "/retronote help" or "/retronote h"
     if command_params[0] == 'help' or command_params[0] == 'h':
@@ -46,29 +47,36 @@ def main():
         show_retronotes(response_url, retro_file_name)
 
     ### When user invokes "/retronote clear {SECRET PASSWORD}"
-    elif len(command_params) == 2 and command_params[0] == 'clear' and command_params[1] == clear_notes_pwd:
-        if is_file_exists(retro_file_name):
-            os.remove(retro_file_name)
-            requests.post(response_url, json=view_templates.get_simple_message_view("Зачистка произведена :wink:"))
-        else:
-            requests.post(response_url, json=view_templates.get_simple_message_view("Удалять нечего. Заметок ещё нет :eyes:"))
+    elif command_params[0] == 'clear':
+        #len(command_params) == 2 and command_params[0] == 'clear' and command_params[1] == clear_notes_pwd:
+        if len(command_params) == 1:
+            requests.post(response_url, json=view_templates.get_simple_message_view("Секретный пароль указать нужно :wink:"))
+        if len(command_params) == 2 and command_params[1] != clear_notes_pwd:
+            requests.post(response_url, json=view_templates.get_simple_message_view("Секретный пароль у админа спросить нужно :wink:"))
+        if len(command_params) == 2 and command_params[1] == clear_notes_pwd:
+            if is_file_exists(retro_file_name):
+                os.remove(retro_file_name)
+                requests.post(response_url, json=view_templates.get_simple_message_view("Зачистка произведена :wink:"))
+            else:
+                requests.post(response_url, json=view_templates.get_simple_message_view("Удалять нечего. Заметок ещё нет :eyes:"))
 
     ### When user invokes "/retronote download"
-    elif command_params[0] == 'download':
+    elif len(command_params) == 1 and command_params[0] == 'download':
         if is_file_exists(retro_file_name):
             with open(retro_file_name, "r") as retrofile:
                 retronotes = retrofile.read()
-            data = {
-                "channels": channel_id,
-                "title": "retronotes.txt",
-                "initial_comment": "Файл со всеми созданными заметками для ретро готов к скачиванию",
-                "filename": "retronotes.txt",
-                "filetype": "text",
-                "content": retronotes
-            }
-            requests.post('https://slack.com/api/files.upload', data=data,
-                headers={'Authorization': 'Bearer {}'.format(bot_access_token)})
+                sorted_retronotes = get_sorted_notes_string(retronotes)
+            upload_file_to_channel(channel_id, filetitle="retronotes.txt", filename="retronotes.txt", filetype="text", filecontent=sorted_retronotes)
+        else:
+            requests.post(response_url, json=view_templates.get_simple_message_view("Нечего скачивать. Заметок ещё нет :eyes:"))
 
+    ### When user invokes "/retronote download csv"
+    elif len(command_params) == 2 and command_params[0] == "download" and command_params[1] == "csv":
+        if is_file_exists(retro_file_name):
+            with open(retro_file_name, "r") as retrofile:
+                retronotes = retrofile.read()
+                csv_content = get_csv_file_content(retronotes)
+            upload_file_to_channel(channel_id, filetitle="retronotes.csv", filename="retronotes.csv", filetype="csv", filecontent=csv_content)
         else:
             requests.post(response_url, json=view_templates.get_simple_message_view("Нечего скачивать. Заметок ещё нет :eyes:"))
 
@@ -77,6 +85,19 @@ def main():
         requests.post(response_url, json=view_templates.get_main_view())
 
     return ""
+
+
+def upload_file_to_channel(channel_id, filetitle, filename, filetype, filecontent):
+    data = {
+                "channels": channel_id,
+                "title": filetitle,
+                "initial_comment": "Файл со всеми созданными заметками для ретро готов к скачиванию",
+                "filename": filename,
+                "filetype": filetype,
+                "content": filecontent
+            }
+    requests.post('https://slack.com/api/files.upload', data=data,
+        headers={'Authorization': 'Bearer {}'.format(bot_access_token)})
 
 
 ### Method is invoked when any interactions with shortcuts, modals, or
@@ -116,7 +137,7 @@ def process_message_action():
          ### When user clicks "View notes" button in message
         if payload["actions"][0]["value"] == "view_notes":
             channel_id = payload["container"]["channel_id"]
-            retro_file_name = get_retrofile_name(channel_id)
+            retro_file_name = rfm.get_retrofile_name(channel_id)
             show_retronotes(response_url, retro_file_name)
 
     ### When user clicks "Add as a retronote" shortcut
@@ -168,7 +189,7 @@ def process_view_submission(payload):
     note_text = state_values["note_description_block"]["note_description"]["value"]
     private_metadata = payload["view"]["private_metadata"]
     channel_id = private_metadata.split(":")[1]
-    retro_file_name = get_retrofile_name(channel_id)
+    retro_file_name = rfm.get_retrofile_name(channel_id)
 
     note_type_text = ""
     if note_type_code == "note_type_good":
@@ -212,17 +233,13 @@ def show_retronotes(response_url, retro_file_name):
 
 def show_help(response_url):
     help_message = '''Команда `/retronote` позволяет управлять заметками для ретро: \n
-        - `/retronote help (h)` - посмотреть help по возможностям команды и формату использования\n
-        - `/retronote add` - добавить заметку для ретро \n
+        - `/retronote help(h)` - посмотреть help по возможностям команды и формату использования\n
+        - `/retronote add {text}` - добавить заметку для ретро \n
         - `/retronote list` - просмотреть все созданные заметки для ретро \n
-        - `/retronote download` - скачать текстовый файл со всеми созданными заметками \n
+        - `/retronote download {csv}` - скачать текстовый файл со всеми созданными заметками \n
         - `/retronote clear` - удалить все заметки _(опция доступна только админам)_ '''
 
     requests.post(response_url, json=view_templates.get_simple_message_view(help_message, replace_original=False))
-
-
-def get_retrofile_name(channel_id):
-    return "ch_" + str(channel_id) + ".txt"
 
 
 def is_file_exists(filename):
@@ -239,7 +256,7 @@ def get_notes_string(note_list_title, notes_list):
         return ""
 
     notes_string = ""
-    notes_string += note_list_title
+    notes_string += note_list_title + ':'
     notes_string += "\n\n"
     for note in notes_list:
         notes_string += "***" + "\n"
@@ -252,11 +269,50 @@ def get_notes_string(note_list_title, notes_list):
 def get_sorted_notes_string(all_notes_string):
     notes = all_notes_string.split("***")
 
-    good_notes = [note for note in notes if note.find("Тип: Хорошо") != -1]
-    bad_notes = [note for note in notes if note.find("Тип: Плохо / нужно улучшить") != -1]
+    ### Creating two lists of notes sorted by type and excluding note type. 12 is len of "Тип: Хорошо"
+    ### 28 is len of "Тип: Плохо / нужно улучшить"
+    good_notes = [note[12:] for note in notes if note.find("Тип: Хорошо") != -1]
+    bad_notes = [note[28:] for note in notes if note.find("Тип: Плохо / нужно улучшить") != -1]
 
     good_notes_str = get_notes_string("ЧТО БЫЛО ХОРОШО", good_notes)
     bad_notes_str = get_notes_string("ЧТО БЫЛО ПЛОХО / НУЖНО УЛУЧШИТЬ", bad_notes)
 
-    return good_notes_str + bad_notes_str
+    return (good_notes_str + bad_notes_str).strip()
+
+
+def combine_lists(list1, list2, empty_position_str=""):
+    list1_len = len(list1)
+    list2_len = len(list2)
+    combined_list = []
+    for i in range(max(list1_len, list2_len)):
+        if i < list1_len and i < list2_len:
+            combined_list.append((list1[i], list2[i]))
+        elif i < list1_len:
+            combined_list.append((list1[i], empty_position_str))
+        elif i < list2_len:
+            combined_list.append((empty_position_str, list2[i]))
+    return combined_list
+
+
+def extract_and_clean_notes(list_of_notes):
+    extracted_notes = [note[note.find("Описание:") + 10: note.find("Добавлена:") - 1] for note in list_of_notes]
+    cleaned_notes = [note.replace("\n", "; ") for note in extracted_notes]
+    cleaned_notes = [note.replace(",", ";") for note in cleaned_notes]
+    return cleaned_notes
+
+
+def get_csv_file_content(text, csv_delimiter=","):
+    notes = text.split("***")
+
+    good_notes = [note[12:] for note in notes if note.find("Тип: Хорошо") != -1]
+    bad_notes = [note[28:] for note in notes if note.find("Тип: Плохо / нужно улучшить") != -1]
+
+    clean_good_notes = extract_and_clean_notes(good_notes)
+    clean_bad_notes = extract_and_clean_notes(bad_notes)
+
+    combined_notes = combine_lists(clean_good_notes, clean_bad_notes)
+    csv_content_str = "good notes,bad notes" + "\n"
+    for good_note, bad_note in combined_notes:
+        csv_content_str += "{}{}{}".format(good_note, csv_delimiter, bad_note) + "\n"
+    return csv_content_str.strip()
 
---
+++ retronote_file_manage.py
@@ -0,0 +1,3 @@
+def get_retrofile_name(channel_id):
+    return "ch_" + str(channel_id) + ".txt"
+
